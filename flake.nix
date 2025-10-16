3ef2733a7f4ec4f3555e9246f63872039625f001
Rasmus Rendal <r.kjaer@famedly.com>
2025-10-16 12:05:08 +0200

    feat: Get flake compiling slint example

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      fenix,
+      nixpkgs,
+    }:
+    {
+
+      devShells.x86_64-linux.default =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            fenix.packages.x86_64-linux.minimal.toolchain
+
+            # Slint
+            eudev.out
+            fontconfig.lib
+            freetype.out
+            libgcc.lib
+            libinput.out
+            libxkbcommon.out
+            mesa.out
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+          ];
+
+          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
+            with pkgs;
+            lib.makeLibraryPath [
+              wayland
+              libxkbcommon
+              fontconfig
+              libGL
+            ]
+          }";
+        };
+
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    };
+}
