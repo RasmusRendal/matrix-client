2402afc091551cecdcefc86106e4d6ccb672eb02
Rasmus Rendal <r.kjaer@famedly.com>
2025-10-17 17:03:02 +0200

    feat: receive multiple messages

--- src/main_window.rs
+++ src/main_window.rs
@@ -1,43 +1,31 @@
-use std::{sync::Arc, time::Duration};
+use std::rc::Rc;
+use std::sync::Arc;
 
 use matrix_sdk::{config::SyncSettings, ruma::events::room::message::SyncRoomMessageEvent};
+use slint::{Model as _, ModelRc, ToSharedString as _, VecModel};
 use tokio::runtime::Runtime;
 
 slint::include_modules!();
 
 pub fn run_main_window(rt: Arc<Runtime>, client: matrix_sdk::Client) {
-    println!(
-        "run main window: we are in thread {:?}",
-        std::thread::current().id()
-    );
-    assert!(client.matrix_auth().logged_in());
     let window = MainWindow::new().unwrap();
     let weak: slint::Weak<MainWindow> = window.as_weak();
 
+    // let model = ModelRc::new(VecModel::from_slice(&[]));
+    // window.set_list_of_messages(model);
+
     client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
-        println!("Received a message {:?}", ev);
         if let Some(event) = ev.as_original() {
             let body = event.content.body().to_owned();
-            println!(
-                "event handler: we are in thread {:?}",
-                std::thread::current().id()
-            );
-            // weak.upgrade_in_event_loop(move |window2| {
-            //     println!(
-            //         "writing message: we are in thread {:?}",
-            //         std::thread::current().id()
-            //     );
-
-            //     window2.invoke_add_message(body.into());
-            // })
-            // .unwrap();
-            slint::invoke_from_event_loop(move || {
-                println!(
-                    "event loop: we are in thread {:?}",
-                    std::thread::current().id()
-                );
-                let window2 = weak.upgrade().unwrap();
-                window2.invoke_add_message(body.into());
+            let message = MessageModel {
+                sender: event.sender.to_shared_string(),
+                message: body.into(),
+            };
+            weak.upgrade_in_event_loop(move |window2| {
+                let mut messages: Vec<MessageModel> =
+                    window2.get_list_of_messages().iter().collect();
+                messages.push(message);
+                window2.set_list_of_messages(ModelRc::new(VecModel::from(messages)));
             })
             .unwrap();
         }
@@ -49,5 +37,6 @@ pub fn run_main_window(rt: Arc<Runtime>, client: matrix_sdk::Client) {
     });
     window.show().unwrap();
     let wb = Box::new(window);
+    // TODO: This is terrible
     Box::leak(wb);
 }
